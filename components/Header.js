09bed31ab894640fb9f9e97d728ffc1337186470
S David Kong <SDAVIDKONG@gmail.com>
2022-10-12 13:19:53 -0400

    fixed header styling

--- components/Header.js
+++ components/Header.js
@@ -5,17 +5,15 @@ import styles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div className={styles.header}>
-      <div>
-        <div className={styles.connectButtonDiv}>
-          <ConnectButton className={styles.button} moralisAuth={false} />
-        </div>
-
-        <img
-          className={styles.lottoTimeBanner}
-          src="/lottotime.png"
-          alt="A colorful banner that reads Lotto Time!"
-        />
+      <div className={styles.connectButtonDiv}>
+        <ConnectButton className={styles.button} moralisAuth={false} />
       </div>
+
+      <img
+        className={styles.png}
+        src="/lottotime.png"
+        alt="A colorful banner that reads Lotto Time!"
+      />
     </div>
   );
 };
